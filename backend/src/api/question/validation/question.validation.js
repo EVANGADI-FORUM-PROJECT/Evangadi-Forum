37679144d2b47e7f665e24bc7f95dc15fcb6e2e6
daniyacodes~ <daniyaasefa01@gmail.com>
2026-09-16 15:27:43 +0300

    createQuestionvalidation  function updated

--- backend/src/api/question/validation/question.validation.js
+++ backend/src/api/question/validation/question.validation.js
@@ -2,6 +2,27 @@ import { body, query, param } from "express-validator";
 import { validationErrorHandler } from "../../../middleware/validation-handler.js";
 
 //[T-9]
+export const createQuestionValidation = [
+    
+    body("title")
+        .notEmpty()
+        .withMessage("Question title is required")
+        .isString()
+        .withMessage("Question title must be a string")
+        .isLength({ min: 5, max: 255 })
+        .withMessage("Question title must be between 5 and 255 characters")
+        .trim(),
+    
+    body("content")
+        .notEmpty()
+        .withMessage("Question content is required")
+        .isString()
+        .withMessage("Question content must be a string")
+        .isLength({ min: 10 })
+        .withMessage("Question content must be at least 10 characters")
+        .trim(),
+    validationErrorHandler,
+]
 
 //[T-10]
 export const getQuestionsValidation = [
