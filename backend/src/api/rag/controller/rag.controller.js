06010ec2474ac2f7354605161d45875790ada2f2
daniyacodes~ <daniyaasefa01@gmail.com>
2026-09-22 17:55:23 +0300

    [T-23] Implement RAG document semantic search- controller

--- backend/src/api/rag/controller/rag.controller.js
+++ backend/src/api/rag/controller/rag.controller.js
@@ -22,10 +22,18 @@ export const createDocumentController = async (req, res, next) => {
 
 export const searchInDocumentController = async (req, res, next) => {
   try {
-const documentId = req.params.documentId;
+        const documentId = req.params.documentId;
         const searchQuery = req.query.query;
         const k = req.query.k ? Number(req.query.k) : undefined;
         const userId = req.user.id;
+
+        const results = await searchInDocumentService(
+            documentId,
+            searchQuery,
+            k,
+            userId
+        );
+        
   } catch (error) {
     
   }
