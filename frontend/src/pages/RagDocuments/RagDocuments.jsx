d3c809d81c0a6e772ca8dc89940c632473093727
zeleke21977 <zolakid50@gmail.com>
2026-09-22 18:23:25 +0300

    [FE] Implement RAG Knowledge Base page

--- frontend/src/pages/RagDocuments/RagDocuments.jsx
+++ frontend/src/pages/RagDocuments/RagDocuments.jsx
@@ -6,6 +6,9 @@ import DocumentList from "./DocumentList";
 import RagAskAI from "./RagAskAI";
 import RagPreview from "./RagPreview";
 import RagSearch from "./RagSearch";
+
+// While a document is still being processed the list is refreshed this often.
+const POLL_INTERVAL_MS = 3000;
 export default function RagDocuments() {
   // TODO [FE-RAG]: Implement the Milestone 3 RAG Documents page.
   return <div>TODO: Implement RAG Documents page</div>;
