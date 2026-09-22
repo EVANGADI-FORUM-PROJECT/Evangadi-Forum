de4fb611ad830f174d3b5810838e792b639e5520
zeleke21977 <zolakid50@gmail.com>
2026-09-22 18:12:42 +0300

    [FE] Implement RAG Knowledge Base page

--- frontend/src/pages/RagDocuments/DocumentListItem.jsx
+++ frontend/src/pages/RagDocuments/DocumentListItem.jsx
@@ -1,6 +1,7 @@
 import { Loader2, Trash2 } from "lucide-react";
 import styles from "./RagDocuments.module.css";
 import StatusBadge from "./StatusBadge";
+/** One row in the library: select button + delete button. */
 export default function DocumentListItem() {
   // TODO [T-24]: Implement document list item.
   return <li>TODO: Implement document list item</li>;
