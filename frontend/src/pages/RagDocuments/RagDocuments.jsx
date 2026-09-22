6ad79da89b9c31b8fee73dbbfb2013f4e42b06a2
zeleke21977 <zolakid50@gmail.com>
2026-09-22 18:22:22 +0300

    [FE] Implement RAG Knowledge Base page

--- frontend/src/pages/RagDocuments/RagDocuments.jsx
+++ frontend/src/pages/RagDocuments/RagDocuments.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { FileText, FileUp, Upload } from "lucide-react";
 import { ragService } from "../../services/rag/rag.service.js";
 import styles from "./RagDocuments.module.css";
+import DocumentList from "./DocumentList";
 export default function RagDocuments() {
   // TODO [FE-RAG]: Implement the Milestone 3 RAG Documents page.
   return <div>TODO: Implement RAG Documents page</div>;
